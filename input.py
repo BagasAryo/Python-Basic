593e0bf650e0cff80eb10390559021e3fd007dae
Bagas Aryo <bagas.priyatama04@gmail.com>
2023-10-22 17:28:58 +0700

    input

---
+++ input.py
@@ -0,0 +1,14 @@
+# episode input user
+
+data = input("Masukkan: ")
+print("data = ", data, "type = ", type(data))
+
+# input int
+
+angka = int(input("Masukkan angka: "))
+print("angka yang dimasukkan = ", angka)
+
+# input bool
+
+boolean = bool(int(input("masukkan boolean: ")))
+print("boolean yang dimasukkann = ", boolean)
