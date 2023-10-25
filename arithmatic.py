76d22a3b324f55eaf97402ac2a21b619df431026
Bagas Aryo <bagas.priyatama04@gmail.com>
2023-10-26 00:08:34 +0700

    operating aritmetic

---
+++ arithmatic.py
@@ -0,0 +1,36 @@
+# operasi aritmatika
+
+a = 14
+b = 3
+
+# tambah +
+hasil = a + b
+print(a, " + ", b, " = ", hasil)
+
+# kurang - 
+hasil = a - b
+print(a, " - ", b, " = ", hasil)
+
+# kali *
+hasil = a * b
+print(a, " * ", b, " = ", hasil)
+
+# bagi /
+hasil = a / b
+print(a, " + ", b, " = ", hasil)
+
+# modulus %
+hasil = a % b
+print(a, " % ", b, " = ", hasil)
+
+# pangkat **
+hasil = a ** b
+print(a, '**', b, '=', hasil)
+
+# floor division //
+hasil = a // b
+print(a, "//", b, "=", hasil)
+
+# prioritas operasi, 
+hasil = a ** b + a - b / a * a // b
+print(a, '**', b, '+',a, '-', b, '/', a, '*', a, '//', b, " = ", hasil)
