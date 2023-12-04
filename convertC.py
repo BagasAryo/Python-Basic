42c2f42b82443339b97d77b735ee876d9de2b2f9
Bagas Aryo <bagas.priyatama04@gmail.com>
2023-12-04 22:36:04 +0700

    unfinished convert

---
+++ convertC.py
@@ -0,0 +1,19 @@
+print("KONVERSI SUHU")
+
+cel = float(input("masukkan nilai celcius : "))
+
+# konversi celcius ke reamur
+
+reamur = float(4/5*cel)
+
+print("nilai konversi celcius ke reamur adalah: ", reamur, "reamur")
+
+# konversi celcius ke fahrenheit
+
+fahren = float(9/5*cel+32)
+
+print("nilai konversi celcius ke reamur adalah: ", fahren, "fahrenheit")
+
+
+# cel = float(input("masukkan nilai celcius : "))
+# cel = float(input("masukkan nilai celcius : "))
