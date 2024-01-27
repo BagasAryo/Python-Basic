15a840e13557ead9ad93cbf095b0d3163fb02074
Bagas Aryo <bagas.priyatama04@gmail.com>
2024-01-27 15:35:33 +0700

    added convertion of kelvin to fahrenheit

--- convertC.py
+++ convertC.py
@@ -1,4 +1,4 @@
-print("KONVERSI SUHU")
+print('\nKONVERSI SUHU')
 
 cel = float(input("masukkan nilai celcius : "))
 
@@ -18,6 +18,14 @@ print("nilai konversi celcius ke reamur adalah: ", fahren, "fahrenheit")
 
 Fahre2 = float(input('masukkan nilai fahrenheit: '))
 
-convFahre = float((5/9*(Fahre2-32))+273)
+convFahre = float((5/9 * (Fahre2 - 32)) + 273)
 
 print('nilai konversi fahrenheit ke kelvin adalah: ', convFahre, 'kelvin')
+
+# konversi kelvin ke fahrenheit
+
+Kelvin2 = float(input('masukkan nilai kelvin: '))
+
+convKelv = float(9/5 * (Kelvin2 - 273) + 32 )
+
+print("nilai konversi kelvin ke fahrenheit adalah: ", convKelv, "fahrenheit\n")
