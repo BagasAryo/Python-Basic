0175d2705684d5b6b738b0d16941098ec983db4c
Bagas Aryo <bagas.priyatama04@gmail.com>
2024-02-09 15:25:38 +0700

    added file two types of looping, for and while

---
+++ loop.py
@@ -0,0 +1,24 @@
+# FOR LOOPING
+# by list
+num = [1, 3, 5, 7, 9]
+for i in num:
+    print(f'angka pertama by list: {i}')
+print('\n')
+
+# by range
+for j in range(1, 10):
+    print(f'angka pertama by range: {j}')
+print('\n')
+
+# by string
+data_str = 'kece badaiii'
+for huruf in data_str:
+    print(huruf)
+print('\n')
+
+# WHILE LOOPING
+angka = 5
+while angka > 0:
+    angka -= 1
+    print(f'angka sekarang adalah {angka}')
+print('cukup')
