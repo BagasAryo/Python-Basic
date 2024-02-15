f0a625db7c1f372a387cc940aff17e372ef84bf3
Bagas Aryo <bagas.priyatama04@gmail.com>
2024-02-15 12:46:46 +0700

    added a bunch of looping task

--- continuepassbreak.py
+++ continuepassbreak.py
@@ -6,11 +6,11 @@ while angka < 3:
     angka += 1
     if angka == 2:
         pass
-        # print('love u')
+        print('love u')
     print(angka)
 print('\n')
 
-# 2. continue -> berfungsi sebagai
+# 2. continue -> berfungsi untuk menunda perintah yang berada setelah continue yang berada di satu perulangan dan berpindah ke iterasi selanjutnya
 kelas = 7
 while kelas > 2:
     kelas -= 1
@@ -22,7 +22,7 @@ while kelas > 2:
 print('keluar while')
 print('\n')
 
-# 3. Break
+# 3. Break -> berfungsi untuk mengakhiri/keluar dari suatu looping yang sekarang dan melanjutkan perintah selanjutnya
 angkaBreak = 0
 while angkaBreak < 5:
     angkaBreak += 1
@@ -32,3 +32,53 @@ while angkaBreak < 5:
         break
     print('apakabar')
 print('akhiru kalam')
+print('\n')
+
+# 4.1. latihan perulangan menggunakan for
+print('====LATIHAN LOOPING====')
+colfor = int(input('masukkan nilai panjang: '))
+rowfor = 1
+for i in range(colfor):
+    print('*' * rowfor)
+    rowfor += 1
+
+# 4.2. latihan perulangan menggunakan while
+colwhile = int(input('masukkan nilai panjang by while: '))
+rowwhile = 1
+while True:
+    print('*' * rowwhile)
+    rowwhile += 1
+    if rowwhile > colwhile:
+        break
+
+# 4.3. latihan perulangan menggunakan while hanya ganjil
+colodd = int(input('masukkan nilai panjang by while odd: '))
+rowodd = 1
+while True:
+    if not(rowodd % 2):
+        rowodd += 1
+        continue
+
+    if rowodd > colodd:
+        break
+
+    print('*' * rowodd)
+    rowodd += 1
+
+# 4.4 latihan perulangan menggunakan while mid
+print('\nWHILE PYRAMID')
+colmid = 10
+rowmid = 1
+space = int(colmid/2)
+
+while True:
+    if not(rowmid % 2):
+        rowmid += 1
+        continue
+
+    if rowmid > colmid:
+        break
+
+    print(' '*space,'*' * rowmid)
+    space -= 1
+    rowmid += 1
