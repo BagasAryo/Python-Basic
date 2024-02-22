5a80048b1d78e89dab3841a401f383c8a3475f59
Bagas Aryo <bagas.priyatama04@gmail.com>
2024-02-22 14:25:06 +0700

    difference between list, tuple, and set

---
+++ tuples.py
@@ -0,0 +1,17 @@
+# TUPLES AND SETS
+
+# list
+data_list = [1, 2, 5, 6, 7, 3]
+print(data_list)
+
+# tuples
+data_tuples = (1, 2, 9, 5, 6, 3)
+# tidak bisa diganti salah satu datanya, ditambah, dihilangkan, dsb
+print(data_tuples)
+# no append
+print(data_tuples[1])
+
+# sets (himpunan)
+data_sets = {1, 2, 6, 9, 7, 3}
+print(data_sets)
+# hanya tidak bisa mengambil/menampilkan satu data, seperti ini -> print(data_sets[3])
