9abe9d37834736b5d809c94c2488b3d99355d9d2
Bagas Aryo <bagas.priyatama04@gmail.com>
2024-02-24 12:45:31 +0700

    added dictionary initially file

---
+++ dictionary/dict_init.py
@@ -0,0 +1,18 @@
+# dictionary init
+data_list = ['bagas', 'aryo', 'irans']
+
+print(data_list[2])
+
+# dictionary (dict) -> associative array
+# identifier -> key
+
+data_dict = {
+    'bg': 'bagas',
+    'ry': 'aryo',
+    'dream': 'irans',
+    'cakep': 10000
+}
+
+print(data_dict['bg'])
+print(data_dict['dream'])
+print(data_dict['cakep'])
